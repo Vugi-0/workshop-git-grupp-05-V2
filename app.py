9b8d87832faefcbbe1c755deb41c3cd620d41db4
Zahron <Tungtonic@proton.me>
2026-03-24 16:15:26 +0100

    Import sub() and handle choice 2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from calculator import add, sub
+
 def run():
     print("1) Add\n Subtract\n0) Exit")
 
@@ -6,3 +8,5 @@ choice = input("Select: ")
 if choice == "1": print("TODO Add")
 a = float(input("a: "))
 b = float(input("b: "))
+
+if choice == "2": print(sub (a, b))

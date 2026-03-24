a1f34f04427e250e433b1d71cdc97fa7e2eb847e
Kai <120798946+XxbudixX@users.noreply.github.com>
2026-03-24 15:57:17 +0100

    Menu: add Multiplay to options

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 def run():
-    print("1) Add\n0) Exit")
+    print("1) Add\n2) Subtract\n3) Multiply\n0) Exit")
 
-<<<<<<< Updated upstream
 choice = input("Select: ")
-=======
-choice = input("Choice: ")
-
->>>>>>> Stashed changes

f1d41799b285691d26e86667439685b94a9bddcd
mohamadsubhihajjar <mohamadsubhi.hajjar@gmail.com>
2026-03-24 16:25:14 +0100

    Update app.py
    Guard: handle division by zero

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from calculator import add, sub, mul, div
-
 def run():
-    print("1) Add\n Subtract\n0) Exit")
+    print("1) Add\n0) Exit")
 
 choice = input("Select: ")
+if choice == '4' and b == '0': 
+    print('error : division by zero')
 
-if choice == "1": print("TODO Add")
-a = float(input("a: "))
-b = float(input("b: "))
+if choice == '4' and b != '0':
+    print(div (a,b))
 
-if choice == "2": print(sub (a, b))
 
-if choice == "3": print(mul (a, b))
 
-if choice == "4": print(div (a, b))

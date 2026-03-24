90b256884a9fecc27262ba808437fa969ef11010
Vugi-0 <viggobengtsson88@gmail.com>
2026-03-24 15:50:32 +0100

    Menu: add Subtract to printed options

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 def run():
-    print("1) Add\n0) Exit")
+    print("1) Add\n Subtract\n0) Exit")
 
-<<<<<<< Updated upstream
 choice = input("Select: ")
-=======
-choice = input("Choice: ")
-
->>>>>>> Stashed changes

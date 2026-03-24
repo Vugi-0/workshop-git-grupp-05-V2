f3eb5119d309ee35a9f4764ff0c3664369daf8cb
mohamadsubhihajjar <mohamadsubhi.hajjar@gmail.com>
2026-03-24 16:09:16 +0100

    Update app.py
    call ad(a,b) and print result

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 def run():
-    print("1) Add\n Subtract\n0) Exit")
+    print("1) Add\n0) Exit")
+
+print(add(a,b))
 
 choice = input("Select: ")
 
-if choice == "1": print("TODO Add")
-a = float(input("a: "))
-b = float(input("b: "))
